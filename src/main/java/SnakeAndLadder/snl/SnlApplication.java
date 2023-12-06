f53d3c6241c96e7f70e93138930ac0d48145115d
Naga Prajwal <nagaprajwal@Nagas-MacBook-Pro.local>
2023-12-06 19:18:47 +0530

    initial commit

---
+++ src/main/java/SnakeAndLadder/snl/SnlApplication.java
@@ -0,0 +1,13 @@
+package SnakeAndLadder.snl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SnakeAndLadder/snl/SnlApplicationTests.java
@@ -0,0 +1,13 @@
+package SnakeAndLadder.snl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
